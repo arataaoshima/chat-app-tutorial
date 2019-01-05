a115fac3f2d66ef0c92171759a63d17bd7947ec6
ARATA <newbluelight1031@gmail.com>
2019-01-05 22:59:41 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'chatroom#index'
+  get "/login" => 'sessions#new'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
