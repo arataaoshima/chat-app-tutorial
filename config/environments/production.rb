49e8189cdfdd28a082cb3015ce7a2ef8e38b1033
ARATA <newbluelight1031@gmail.com>
2019-01-09 02:33:57 +0000

    tt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,10 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   
-  config.web_socket_server_url = 'wss://polar-ravine-77342.herokuapp.com/cable'
-  #config.action_cable.url = 'wss://polar-ravine-77342.herokuapp.com/cable'
-  #config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]
-  config.action_cable.allowed_request_origins = ['https://polar-ravine-77342.herokuapp.com', 'https://polar-ravine-77342.herokuapp.com']
+
   
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -48,8 +45,13 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+   config.action_cable.url = 'wss://polar-ravine-77342.herokuapp.com/cable'
+   config.action_cable.allowed_request_origins = [ 'https://polar-ravine-77342.herokuapp.com', '/https?:\/\/.*/' ]
+   
+     #config.web_socket_server_url = 'wss://polar-ravine-77342.herokuapp.com/cable'
+     #config.action_cable.url = 'wss://polar-ravine-77342.herokuapp.com/cable'
+     #config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]
+  config.action_cable.allowed_request_origins = ['https://polar-ravine-77342.herokuapp.com', 'https://polar-ravine-77342.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
